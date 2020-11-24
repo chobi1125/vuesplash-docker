328129ca94522a95599b768df07135af86978e26
chobi1125 <chonmage112000@yahoo.co.jp>
2020-11-24 23:20:28 +0900

    認証保持まで※６章

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ composer create-project "laravel/laravel=6.*" .
 ## 覚書
 サーバーの接続
 http://0.0.0.0:8081/
+
+DBへの接続(Docker用)
+```
+DB_CONNECTION=pgsql
+DB_HOST=vuesplash_database
+DB_PORT=5432
+DB_DATABASE=postgres
+DB_USERNAME=postgres
+DB_PASSWORD=secret
+```
--- web/routes/web.php
+++ web/routes/web.php
@@ -2,4 +2,9 @@
 
 // APIのURL以外のリクエストに対してはindexテンプレートを返す
 // 画面遷移はフロントエンドのVueRouterが制御する
-Route::get('/{any?}', fn() => view('index'))->where('any', '.+'); // PHP7.4の記法
+// Route::get('/{any?}', fn() => view('index'))->where('any', '.+'); // PHP7.4の記法
+
+Route::get('/{any?}', function(){
+    return view('index');
+    // ->where('any', '.+');
+});
